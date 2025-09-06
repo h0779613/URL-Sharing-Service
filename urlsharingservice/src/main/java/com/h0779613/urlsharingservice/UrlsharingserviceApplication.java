67ccc0965208d3bb2d5c5e17e4758a07d1846b0b
Ito Hisamitsu <h0779613@gmail.com>
2025-09-06 09:29:40 +0900

    プロジェクト初期化

---
+++ urlsharingservice/src/main/java/com/h0779613/urlsharingservice/UrlsharingserviceApplication.java
@@ -0,0 +1,13 @@
+package com.h0779613.urlsharingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlsharingserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrlsharingserviceApplication.class, args);
+	}
+
+}
---
+++ urlsharingservice/src/test/java/com/h0779613/urlsharingservice/UrlsharingserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.h0779613.urlsharingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrlsharingserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
